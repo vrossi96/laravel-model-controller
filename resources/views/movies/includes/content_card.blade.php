8eff722ad53662d73d59cbf679fb344bfa089d0c
vrossi96 <vrossi396@yahoo.it>
2022-03-17 17:12:07 +0100

    add dynamic rating class

--- resources/views/movies/includes/content_card.blade.php
+++ resources/views/movies/includes/content_card.blade.php
@@ -1,6 +1,6 @@
 <div class="card-header">
    <h5>{{ $movie->title }}</h2>
-      <div>{{ $movie->vote }}</div>
+      <div class="{{ $movie->vote > 8 ? 'nice' : 'good' }}">{{ $movie->vote }}</div>
 </div>
 <div class="card-body">
    <p>IT: {{ $movie->title }}</p>
